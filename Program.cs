b81e3184f1f99882182acd96f44d46eaaf4339e7
Chengming <v-cruan@microsoft.com>
2023-08-21 11:15:28 +0800

    delete useless code

--- Program.cs
+++ Program.cs
@@ -1,28 +1,26 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure.Management.Compute.Fluent;
-using Microsoft.Azure.Management.Compute.Fluent.Models;
-using Microsoft.Azure.Management.CosmosDB.Fluent;
-using Microsoft.Azure.Management.CosmosDB.Fluent.Models;
-using Microsoft.Azure.Management.Fluent;
-using Microsoft.Azure.Management.Network.Fluent;
-using Microsoft.Azure.Management.Network.Fluent.Models;
-using Microsoft.Azure.Management.ResourceManager.Fluent;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
-using Microsoft.Azure.Management.ResourceManager.Fluent.Core.ResourceActions;
-using Microsoft.Azure.Management.Samples.Common;
-using Microsoft.Rest.Azure;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager.Resources.Models;
+using Azure.ResourceManager.Samples.Common;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager;
+using Azure.ResourceManager.CosmosDB.Models;
+using Azure.ResourceManager.CosmosDB;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents;
 
 namespace CosmosDBTableWithVirtualNetworkRule
 {
     public class Program
     {
+        private static ResourceIdentifier? _resourceGroupId = null;
+        private const int _maxStalenessPrefix = 100000;
+        private const int _maxIntervalInSeconds = 300;
+
         /**
          * Azure CosmosDB sample for using Virtual Network ACL rules.
          *  - Create a Virtual Network with two subnets.
@@ -32,14 +30,21 @@ namespace CosmosDBTableWithVirtualNetworkRule
          *  - Delete a virtual network.
          *  - Delete the CosmosDB.
          */
-        public static void RunSample(IAzure azure)
+        public static async Task RunSample(ArmClient client)
         {
-            string cosmosDBName = SdkContext.RandomResourceName("cosmosdb", 15);
-            string rgName = SdkContext.RandomResourceName("rgcosmosdb", 24);
-            string vnetName = SdkContext.RandomResourceName("vnetcosmosdb", 20);
-
             try
             {
+                // Get default subscription
+                SubscriptionResource subscription = await client.GetDefaultSubscriptionAsync();
+
+                // Create a resource group in the EastUS region
+                string rgName = Utilities.CreateRandomName("CosmosDBTemplateRG");
+                Utilities.Log($"Creating a resource group..");
+                ArmOperation<ResourceGroupResource> rgLro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, new ResourceGroupData(AzureLocation.EastUS));
+                ResourceGroupResource resourceGroup = rgLro.Value;
+                _resourceGroupId = resourceGroup.Id;
+                Utilities.Log("Created a resource group with name: " + resourceGroup.Data.Name);
+
                 // ============================================================
                 // Create a virtual network with two subnets.
                 Console.WriteLine("Creating a CosmosDB...");
@@ -119,25 +124,18 @@ namespace CosmosDBTableWithVirtualNetworkRule
 
                 //============================================================
                 // Delete CosmosDB
-                Console.WriteLine("Deleting the CosmosDB");
-
-                // work around CosmosDB service issue returning 404 CloudException on delete operation
-                try
-                {
-                    azure.CosmosDBAccounts.DeleteById(cosmosDBAccount.Id);
-                }
-                catch (CloudException)
-                {
-                }
-                Console.WriteLine("Deleted the CosmosDB");
+                Utilities.Log("Deleting the CosmosDB");
             }
             finally
             {
                 try
                 {
-                    Utilities.Log("Deleting resource group: " + rgName);
-                    azure.ResourceGroups.BeginDeleteByName(rgName);
-                    Utilities.Log("Deleted resource group: " + rgName);
+                    if (_resourceGroupId is not null)
+                    {
+                        Utilities.Log($"Deleting Resource Group: {_resourceGroupId}");
+                        await client.GetResourceGroupResource(_resourceGroupId).DeleteAsync(WaitUntil.Completed);
+                        Utilities.Log($"Deleted Resource Group: {_resourceGroupId}");
+                    }
                 }
                 catch (NullReferenceException)
                 {
@@ -150,24 +148,20 @@ namespace CosmosDBTableWithVirtualNetworkRule
             }
         }
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             try
             {
                 //=================================================================
                 // Authenticate
-                var credentials = SdkContext.AzureCredentialsFactory.FromFile(Environment.GetEnvironmentVariable("AZURE_AUTH_LOCATION"));
+                var clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+                var clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+                var tenantId = Environment.GetEnvironmentVariable("TENANT_ID");
+                var subscription = Environment.GetEnvironmentVariable("SUBSCRIPTION_ID");
+                ClientSecretCredential credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+                ArmClient client = new ArmClient(credential, subscription);
 
-                var azure = Azure
-                    .Configure()
-                    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
-                    .Authenticate(credentials)
-                    .WithDefaultSubscription();
-
-                // Print selected subscription
-                Utilities.Log("Selected subscription: " + azure.SubscriptionId);
-
-                RunSample(azure);
+                await RunSample(client);
             }
             catch (Exception e)
             {
